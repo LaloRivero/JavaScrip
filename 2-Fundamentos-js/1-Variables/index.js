9d701cf399f652971a4e8739895bef2b99f9cc15
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2019-11-13 18:14:54 -0700

    Strings en JS

--- 2-Fundamentos-js/1-Variables/index.js
+++ 2-Fundamentos-js/1-Variables/index.js
@@ -1,1 +1,7 @@
-var nombre = 'Lalo', apellido = 'Rivero'
+var nombre = 'Lalo', apellido = 'Rivero';
+var Mayus = nombre.toUpperCase() , minus = apellido.toLowerCase();
+var primerLetra = nombre.charAt(0);
+var tam = nombre.length;
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
+var str = nombreCompleto.substr(2, 5);
+var ultima = nombreCompleto.charAt(nombreCompleto.length-1);
