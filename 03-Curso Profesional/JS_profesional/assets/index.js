6030797b3faf88146a110e20a2861255c0cd346b
Eduardo Rivero <lalo@lalorivero.com>
2020-03-20 14:46:14 -0700

    profesional JS

---
+++ 03-Curso Profesional/JS_profesional/assets/index.js
@@ -0,0 +1,14 @@
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "../plugins/AutoPlay.js";
+import AutoPause from "../plugins/AutoPause.js";
+
+const video = document.querySelector("video");
+const playButton = document.querySelector("#playButton");
+const mutedButton = document.querySelector("#muteButton");
+
+const player = new MediaPlayer({
+  el: video,
+  plugins: [new AutoPlay(), new AutoPause()]
+});
+playButton.onclick = () => player.togglePlay();
+mutedButton.onclick = () => player.toggleMuted();
